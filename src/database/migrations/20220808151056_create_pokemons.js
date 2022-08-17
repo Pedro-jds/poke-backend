c8938d49aac0934e550ee34ea9700841ba096a02
Pedro-jds <pedro81565@hotmail.com>
2022-08-16 21:07:22 -0300

    fix: adicionado alguns dados inicias na criação de tabelas via knex

--- src/database/migrations/20220808151056_create_pokemons.js
+++ src/database/migrations/20220808151056_create_pokemons.js
@@ -12,6 +12,12 @@ export function up(knex) {
     table.integer("special_attack", 3).notNullable;
     table.integer("special_defense", 3).notNullable;
     table.integer("speed", 3).notNullable;
+  }).then(() => {
+    return knex("pokemons").insert([
+      { name: "pikachu",hp:50, attack: 20,defense:30,special_attack:60,special_defense:80,speed:90 },
+      { name: "bulbasaur",hp:80, attack: 30,defense:40,special_attack:70,special_defense:90,speed:30 },
+      { name: "charmander",hp:70, attack: 80,defense:20,special_attack:80,special_defense:20,speed:70 }
+    ]);
   });
 }
 
--- src/database/migrations/20220809201406_counter_wins.js
+++ src/database/migrations/20220809201406_counter_wins.js
@@ -4,18 +4,24 @@
  */
 
 export function up(knex) {
-  return knex.schema.createTable("counter_wins", (table) => {
-    table.increments();
-    table.string("player", 50).notNullable;
-    table.integer("wins").notNullable;
-  });
+  return knex.schema
+    .createTable("counter_wins", (table) => {
+      table.increments();
+      table.string("player", 50).notNullable;
+      table.integer("wins").notNullable;
+    })
+    .then(() => {
+      return knex("counter_wins").insert([
+        { player: "playerOne", wins: 0 },
+        { player: "playerTwo", wins: 0 },
+      ]);
+    });
 }
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-
 
 export function down(knex) {
   return knex.schema.dropTable("counter_wins");
